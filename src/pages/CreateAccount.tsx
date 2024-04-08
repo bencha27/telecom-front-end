07f05289acaf10d78b1736a36696a1d70ec69c5b
bencha27 <115957643+bencha27@users.noreply.github.com>
2024-04-08 14:00:32 -0600

    Added create account form

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,1 +1,35 @@
-export {}
+import { Button, Col, Form, Row } from "react-bootstrap";
+
+export default function CreateAccount() {
+  return (
+    <div>
+      <Row className="justify-content-center">
+        <Col xs="10" sm="6" md="5" xl="4" xxl="3">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Enter username" />
+            </Form.Group>
+              
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+              Create account
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
+}
