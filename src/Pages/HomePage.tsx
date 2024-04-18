7198958b92d0e35c49d997c089e9bf6d6f9dec89
Bram <b93ram@gmail.com>
2024-04-18 14:26:33 -0400

    changed the title

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
   return (
     <Container className="py-5">
       <Row className="py-3">
-        <h1 className="text-center fst-italic">What you see is what you get!</h1>
+        <h1 className="text-center fst-italic">A Clear Choice</h1>
       </Row>
 
       <Row className="py-5 justify-content-center">
